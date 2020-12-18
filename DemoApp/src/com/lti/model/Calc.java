1f7e0c347abd97020511bebecc9ced1281e441cd
Sahithi-bellam <sahithibellam@gmail.com>
2020-12-18 16:41:54 +0530

    added mul method from br1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,6 +8,9 @@ public class Calc {
 	public void subs(int num1,int num2) {
 		System.out.println("Difference="+(num1-num2));
 	}
+	public void mul(int num1,int num2) {
+		System.out.println("Multiplication="+(num1*num2));
+	}
 	
 	
 }

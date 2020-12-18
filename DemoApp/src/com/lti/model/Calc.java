375a090e0b8d3e0fdc240047bed4ff7607a27304
Sahithi-bellam <76149001+Sahithi-bellam@users.noreply.github.com>
2020-12-18 16:46:47 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -9,6 +9,9 @@ public class Calc {
 	public void subs(int num1,int num2) {
 		System.out.println("Difference="+(num1-num2));
 	}
+	public void prod(int num1,int num2) {
+		System.out.println("Product="+(num1*num2));
+	}
 	
 	
 }
